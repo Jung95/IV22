84fae41cea307e5aea418bf7c707347299205119
Jongho Jung <jung95219@gmail.com>
2022-11-09 19:58:32 +0100

    simplify codes

--- assignment02/impl/app.js
+++ assignment02/impl/app.js
@@ -21,30 +21,22 @@ function draw(two) {
   //bandwidth h is 0.5
   const h = 0.5
   const n = numbers.length
-  let data = [];
-  //Create the density estimate
-  // i is [0,100] with step size 0.1
-  for (let i = 0; i <= 1000; i++) {
-    let temp = 0;
-    // sum i=1=>n K((x-x_i) / h)
-    for (let j = 0; j < n; j++) {
-      temp = temp + gaussianKDE((numbers[j] - (i * 0.1).toFixed(1) )/ h);
-    }
-    // 1/n*h 
-    data.push((1 / (n*h)) * temp);
-  }
+
   for (let x = 0; x <= 100; x += 0.1) {
     X.push(x);
-    let y = (x*10).toFixed(0)
-    Y.push(data[y]); // TODO: replace by KDE(x)
+    let temp = 0;
+    // sum i=1=>n K((x-x_i) / h)
+    for (let i = 0; i < n; i++) {
+      // K((x-x_i) / h) = K(u) = 1/ sqrt(1/PI)*e^(u^2/-2)
+      // x = numbers[i], x_i = x
+      let kde =  (1 / Math.sqrt(2 * Math.PI)) * Math.exp(Math.pow(( (numbers[i] - x) / h), 2) / -2)
+      temp = temp + kde;
+    }
+    // 1/n*h 
+    Y.push((1 / (n * h)) * temp); // TODO: replace by KDE(x)
   }
 
   drawAxesForTask3(two);
   const path = createPathFromXY(X, Y);
   two.add(path);
-}
-
-function gaussianKDE(u) {
-  // K(u) = 1/ sqrt(1/PI)*e^(u^2/-2)
-  return (1 / Math.sqrt(2 * Math.PI)) * Math.exp(Math.pow(u, 2) / -2);
 }
