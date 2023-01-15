5a9b541befd7747ecadc2595fe9f42b0e02b25ab
Jongho Jung <jung95219@gmail.com>
2023-01-15 20:22:59 +0100

    task3 a)

--- assignment08/impl/app.js
+++ assignment08/impl/app.js
@@ -8,7 +8,8 @@ function getTutorialInfo() {
 }
 
 // feel free to tweak this DURING TESTING
-const quadtreeMaxDepth = 10;
+const quadtreeMaxDepth = 3; //default 10
+const canvasLength = 512;
 
 /**
  * Initialize the quadtree.
@@ -30,8 +31,116 @@ const quadtreeMaxDepth = 10;
  *                                    `getClosestCircles`.
  */
 function initQuadtree(circles, x0, x1, y0, y1) {
+  //console.log(x0, x1, y0, y1);
+  //circles[0].position.x
   // TODO
-  return {};
+  //Calc Now Depth
+  let nowDepth = 0;
+  let length = x1 - x0;
+  while (length < canvasLength) {
+    nowDepth = nowDepth + 1;
+    length = length * 2;
+  }
+  // if now depth is bigger than max depth, retrun leaf-node
+  if (nowDepth > quadtreeMaxDepth) {
+    let node = {
+      isLeaf: true,
+      circles: circles,
+      x0: x0,
+      x1: x1,
+      y0: y0,
+      y1: y1,
+      children: [],
+    };
+    return node;
+  }
+
+  // if number of circle is 0, return leaf-node with zero - circles
+  if (circles.length == 0) {
+    let node = {
+      isLeaf: true,
+      circles: null,
+      x0: x0,
+      x1: x1,
+      y0: y0,
+      y1: y1,
+      children: [],
+    };
+    return node;
+  }
+  //Check Merge
+
+  //Split Quad
+  let subWidth = (x1 - x0) / 2;
+  let subHeight = (y1 - y0) / 2;
+  // index
+  let TopLeftQuad = new Array();
+  let TopRightQuad = new Array();
+  let BottomLeftQuad = new Array();
+  let BottomRightQuad = new Array();
+  circles.forEach((circle) => {
+    //belong Top Left
+    if (
+      circle.position.x <= x0 + subWidth &&
+      circle.position.y <= y0 + subHeight
+    ) {
+      TopLeftQuad.push(circle);
+    }
+    //belong Top Right
+    else if (circle.position.x <= x1 && circle.position.y <= y0 + subHeight) {
+      TopRightQuad.push(circle);
+    }
+    //belong Bottom Left
+    else if (circle.position.x <= x0 + subWidth && circle.position.y <= y1) {
+      BottomLeftQuad.push(circle);
+    }
+    //belong Bottom Top
+    else if (circle.position.x <= x1 && circle.position.y <= y1) {
+      BottomRightQuad.push(circle);
+    }
+  });
+
+  //Top Left
+  let topLeftNode = initQuadtree(
+    TopLeftQuad,
+    x0,
+    x1 - subWidth,
+    y0,
+    y1 - subHeight
+  );
+  //Top Right
+  let topRightNode = initQuadtree(
+    TopRightQuad,
+    x0 + subWidth,
+    x1,
+    y0,
+    y1 - subHeight
+  );
+  //Bottom Left
+  let bottomLeftNode = initQuadtree(
+    BottomLeftQuad,
+    x0,
+    x1 - subWidth,
+    y0 + subHeight,
+    y1
+  );
+  //Bottom Right
+  let bottomRightNode = initQuadtree(
+    BottomRightQuad,
+    x0 + subWidth,
+    x1,
+    y0 + subHeight,
+    y1
+  );
+
+  return {
+    isLeaf: false,
+    x0: x0,
+    x1: x1,
+    y0: y0,
+    y1: y1,
+    children: [topLeftNode, topRightNode, bottomLeftNode, bottomRightNode],
+  };
 }
 
 /**
