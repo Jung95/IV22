5c32dbcbe63b8fddba870058912700638d72ca05
Jongho Jung <jung95219@gmail.com>
2023-01-22 21:25:28 +0100

    impl task2 ass9

--- assignment09/impl/app.js
+++ assignment09/impl/app.js
@@ -22,6 +22,64 @@ function draw(two) {
 }
 
 function getClustering(k, data) {
-  if (k <= 1) return [data];
-  //TODO (a): Perform k-Means clustering
+  /**
+   * get array, that contains k arrays each containing the corresponding data items of the respective cluster.
+   */
+  // get random centroids from data
+  const centroids = getRandomCentroids(k, data);
+  // Assign labels to each datapoint based on centroids
+  // prep data structure:
+  const labels = [];
+  for (let c = 0; c < centroids.length; c++) {
+    labels[c] = [centroids[c]];
+  }
+  // For each element in the dataset, choose the closest centroid.
+  // Make that centroid the element's label.
+  for (let i = 0; i < data.length; i++) {
+    const a = data[i];
+    let closestCentroid, closestCentroidIndex, prevDistance;
+    for (let j = 0; j < centroids.length; j++) {
+      let centroid = centroids[j];
+      if (j === 0) {
+        closestCentroid = centroid;
+        closestCentroidIndex = j;
+        prevDistance = math.distance(a, closestCentroid);
+      } else {
+        // get  distance:
+        const distance = math.distance(a, centroid);
+        if (distance < prevDistance) {
+          prevDistance = distance;
+          closestCentroid = centroid;
+          closestCentroidIndex = j;
+        }
+      }
+    }
+    // push point to centroid labels array:
+    labels[closestCentroidIndex].push(a);
+  }
+  return labels;
+}
+
+// help functions
+
+function getRandomCentroids(k, data) {
+  /**
+   * get random centroids from data
+   */
+
+  const CentroidIndex = new Array();
+  const numSample = data.length;
+  let index;
+  while (CentroidIndex.length < k) {
+    index = getRandomNumber(0, numSample);
+    if (CentroidIndex.indexOf(index) === -1) {
+      CentroidIndex.push(index);
+    }
+  }
+  const centroids = [];
+  for (let i = 0; i < CentroidIndex.length; i++) {
+    const centroid = [...data[CentroidIndex[i]]];
+    centroids.push(centroid);
+  }
+  return centroids;
 }
